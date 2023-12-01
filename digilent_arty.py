29ccdc8ded0d13135de7626bf4d01a2e9eb1f488
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2023-12-01 17:40:54 +0100

    digilent_arty: update to add a SoCRegion with adress > 32bits connected to Arty's RAM.

--- digilent_arty.py
+++ digilent_arty.py
@@ -15,9 +15,11 @@ from litex.gen import *
 from litex_boards.platforms import digilent_arty
 
 from litex.soc.cores.clock import *
+from litex.soc.integration.soc import SoCRegion
 from litex.soc.integration.soc_core import *
 from litex.soc.integration.soc import *
 from litex.soc.integration.builder import *
+from litex.soc.cores.led import LedChaser
 
 from litedram.modules import MT41K128M16
 from litedram.phy import s7ddrphy
@@ -60,7 +62,10 @@ class _CRG(LiteXModule):
 # BaseSoC ------------------------------------------------------------------------------------------
 
 class BaseSoC(SoCCore):
-    def __init__(self, variant="a7-35", toolchain="vivado", sys_clk_freq=100e6, sram_test=False,
+    def __init__(self, variant="a7-35", toolchain="vivado", sys_clk_freq=100e6,
+            sram_test  = False,
+            sdram_test = False,
+        with_led_chaser = True,
         **kwargs):
         platform = digilent_arty.Platform(variant=variant, toolchain=toolchain)
 
@@ -80,8 +85,41 @@ class BaseSoC(SoCCore):
             self.add_sdram("sdram",
                 phy           = self.ddrphy,
                 module        = MT41K128M16(sys_clk_freq, "1:4"),
-                l2_cache_size = kwargs.get("l2_size", 8192)
+                l2_cache_size = 0
             )
+
+            if sdram_test:
+                from litedram.frontend.wishbone import LiteDRAMWishbone2Native
+                # Request a LiteDRAM native port.
+                port = self.sdram.crossbar.get_port()
+                port.data_width = 2**int(log2(port.data_width)) # Round to nearest power of 2.
+
+                # Add SDRAM region.
+                myram_region = SoCRegion(
+                    origin = 0x4_0000_0000,
+                    size   = 0x40_0000,
+                    mode   = "rwx")
+                self.bus.add_region("myram", myram_region)
+
+                # Create Wishbone Slave.
+                wb_sdram = wishbone.Interface(
+                    data_width    = self.bus.data_width,
+                    address_width = self.bus.address_width,
+                    addressing    = "word")
+                self.bus.add_slave(name="myram", slave=wb_sdram)
+
+                litedram_wb = wishbone.Interface(
+                    data_width    = port.data_width,
+                    address_width = port.address_width,
+                    addressing    = "word")
+                self.submodules += wishbone.Converter(wb_sdram, litedram_wb)
+
+                # Wishbone Slave <--> LiteDRAM bridge.
+                self.wishbone_bridge = LiteDRAMWishbone2Native(
+                    wishbone     = litedram_wb,
+                    port         = port,
+                    base_address = self.bus.regions["main_ram"].origin
+                )
 
         # SRAMs ------------------------------------------------------------------------------------
         if sram_test:
@@ -94,6 +132,13 @@ class BaseSoC(SoCCore):
                 size   = 0x100,
             )
 
+        # Leds -------------------------------------------------------------------------------------
+        if with_led_chaser:
+            self.leds = LedChaser(
+                pads         = platform.request_all("user_led"),
+                sys_clk_freq = sys_clk_freq,
+            )
+
 # Build --------------------------------------------------------------------------------------------
 
 def main():
@@ -103,6 +148,7 @@ def main():
     parser.add_target_argument("--variant",      default="a7-35",           help="Board variant (a7-35 or a7-100).")
     parser.add_target_argument("--sys-clk-freq", default=100e6, type=float, help="System clock frequency.")
     parser.add_target_argument("--sram-test",    action="store_true",       help="SRAM test.")
+    parser.add_target_argument("--sdram-test",   action="store_true",       help="SRAM test.")
     args = parser.parse_args()
 
     soc = BaseSoC(
@@ -110,6 +156,7 @@ def main():
         toolchain      = args.toolchain,
         sys_clk_freq   = args.sys_clk_freq,
         sram_test      = args.sram_test,
+        sdram_test     = args.sdram_test,
         **parser.soc_argdict
     )
 
@@ -128,6 +175,8 @@ def main():
 if __name__ == "__main__":
     main()
 
+# SRAM test.
+# ----------
 # ./digilent_arty.py --sram-test --uart-name=uartbone --csr-csv=csr.csv --build --bus-address-width=64 --load
 # litex_server --uart --addr-width=64 --uart-port=/dev/ttyUSB1
 # litex_cli --write 0x400020000 0x12345678
@@ -136,3 +185,21 @@ if __name__ == "__main__":
 # litex_cli --read 0x400020000
 # or
 # ./test_SRAMAccess.py [--ident] [--access] [sram]
+
+# SRAM test.
+# ----------
+# ./digilent_arty.py --sdram-test --uart-name=crossover --with-uartbone --csr-csv=csr.csv --build --bus-address-width=64 --load
+# litex_server --uart --addr-width=64 --uart-port=/dev/ttyUSB1
+
+# Write at > 32bits adr
+# litex_client --write 0x400000000 0x12345678
+# read at > 32bits adr
+# litex_client --read  0x400000000
+# 0x400000000 : 0x12345678
+# read at < 32bits adr
+# litex_client --read  0x40000000
+# 0x40000000 : 0x12345678
+# with bios (< 32bits adr):
+# mem_read 0x40000000
+# Memory dump:
+# 0x40000000  78 56 34 12                                      xV4.
